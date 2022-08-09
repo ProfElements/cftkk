2e322b65608f88caf35d1f4ee41ad693962e58f5
ProfElements <profelements1@gmail.com>
2022-08-09 00:09:48 -0500

    Update and add examples

--- examples/demo2.rs
+++ examples/demo2.rs
@@ -1,5 +1,5 @@
 use cftkk::gcp::Gcp;
-use std::{env, fs};
+use std::{env, fs, path::PathBuf};
 
 fn main() {
     let args = env::args().collect::<Vec<_>>();
@@ -10,7 +10,11 @@ fn main() {
     let data = fs::read(&args[1]).unwrap();
     let gcp = Gcp::new(data).unwrap();
 
+    let mut path = PathBuf::from(&args[1]);
+    path.set_extension("");
+
+    std::fs::create_dir(&path).unwrap();
     for files in gcp.get_files() {
-        std::fs::write(files.name, files.data).unwrap();
+        std::fs::write(path.join(files.name), files.data).unwrap();
     }
 }
